dc266012f3a87780619ecc1726838f90c6f7fbcc
DREW1606 <andrewlr1606@gmail.com>
2024-10-10 20:08:15 -0400

    estilos

---
+++ Pages/Support.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CNSVM.Pages
+{
+    public class SupportModel : PageModel
+    {
+        public void OnGet()
+        {
+            // Puedes agregar lógica aquí si es necesario en el futuro.
+        }
+    }
+}
