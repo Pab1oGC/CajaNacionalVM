3461a3f8f88fb4f4e4ca8f5a522c47cdd53f39b5
DREW1606 <andrewlr1606@gmail.com>
2024-11-11 22:46:53 -0400

    Pagina Support

---
+++ Pages/Support.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CNSVM.Pages
+{
+    public class SupportModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
