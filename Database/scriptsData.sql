1414c6fc5c3ecaa73bc4856c24aa8451a86df832
Pablo Gutierrez P <pablogutierrrez0@gmail.com>
2024-11-19 01:28:25 -0400

    añadiendo las instrucciones para la generacion de la BDD y datos de prueba

---
+++ Database/scriptsData.sql
@@ -0,0 +1,4 @@
+INSERT INTO [User] (Username,PasswordHash,Role,CreatedAt,Status,Name,FirstName,LastName,Email,Specialty)
+VALUES ('DrNico','$2a$12$1vUul6Zml040s.Y1IDpyt.oZSllID8csmUmxAeRguCKw9A2esmKl6','M','2024-07-25 00:00:00.0000000','A','Nicolas','Toco','Yucra','nicolastocoyucra@gmail.com','General')
+--Username: DrNico
+--Password: 12909506
